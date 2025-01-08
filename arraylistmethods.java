0a412deff028098b0d799face8fa5926842df2da
Ganapathi442005 <147175511+Ganapathi442005@users.noreply.github.com>
2025-01-08 10:51:15 +0530

    Create arraylistmethods.java

---
+++ arraylistmethods.java
@@ -0,0 +1,20 @@
+Method	Description
+add(E e)	-Appends the specified element to the end of the list.
+add(int index, E element)-	Inserts the specified element at the specified position in the list.
+addAll(Collection<? extends E> c)-	Appends all elements from the specified collection to the list.
+addAll(int index, Collection<? extends E> c)-	Inserts all elements of the collection starting at the specified index.
+get(int index)-	Returns the element at the specified position.
+set(int index, E element)	-Replaces the element at the specified position with the specified element.
+remove(Object o)-	Removes the first occurrence of the specified element.
+remove(int index)	-Removes the element at the specified position.
+clear()	-Removes all elements from the list.
+contains(Object o)	-Returns true if the list contains the specified element.
+isEmpty()-	Returns true if the list has no elements.
+size()-	Returns the number of elements in the list.
+indexOf(Object o)	-Returns the index of the first occurrence of the specified element.
+lastIndexOf(Object o)	-Returns the index of the last occurrence of the specified element.
+iterator()-	Returns an iterator over the elements in this list.
+listIterator()-	Returns a list iterator over the elements in this list.
+forEach(Consumer<? super E> action)-	Performs the specified action for each element of the list.
+toArray()-	Converts the list to an array.
+toArray(T[] a)-	Converts the list to an array of the specified type.
