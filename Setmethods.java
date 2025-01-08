170da16f7019aef52fc7a9fea87d3b380234629f
Ganapathi442005 <147175511+Ganapathi442005@users.noreply.github.com>
2025-01-08 10:45:14 +0530

    Create Setmethods.java

---
+++ Setmethods.java
@@ -0,0 +1,13 @@
+set methods
+add() - adds the specified element to the set
+addAll() - adds all the elements of the specified collection to the set
+iterator() - returns an iterator that can be used to access elements of the set sequentially
+remove() - removes the specified element from the set
+removeAll() - removes all the elements from the set that is present in another specified set
+retainAll() - retains all the elements in the set that are also present in another specified set
+clear() - removes all the elements from the set
+size() - returns the length (number of elements) of the set
+toArray() - returns an array containing all the elements of the set
+contains() - returns true if the set contains the specified element
+containsAll() - returns true if the set contains all the elements of the specified collection
+hashCode() - returns a hash code value (address of the element in the set)
